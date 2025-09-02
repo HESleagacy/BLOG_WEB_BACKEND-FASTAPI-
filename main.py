934a22f4e9608d701a66db5b6103c553fc37a305
Sarva Dubey <sarvadubey@gmail.com>
2025-09-02 13:43:07 +0530

    Practicing_GIT

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+app = FastAPI()
+@app.get('/blog') # write like /blog?limit=50 as a query
+def index(limit=10, published:bool=True, sort: Optional[str] = None):
+    #Only get given amt published blogs (Default=10)
+    if published:
+        return {'data': f"{limit} blogs"}
+    else:
+        return {'data': f"{limit} from the DB"}
+
+@app.get('/blog/{id}') #Dynamic Route
+def show(id:int):
+    return {'data':id}
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data':{'1','2'}}
+#Schema for REQ(BELOW):
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+##REQ__BODY
+@app.post('/blog')
+def Create_blog():
+    return {'data':"Blog is Created"}
